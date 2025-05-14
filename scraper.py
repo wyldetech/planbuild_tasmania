db14c9c3aee7110c95ef54f1845d225f92c13a20
wyldetech <69891468+wyldetech@users.noreply.github.com>
2025-05-14 11:17:16 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,21 +1,48 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape("http://foo.com")
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+elements = root.cssselect("div.wrapper div.row.advertisement-result-row")
+
+for element in elements:
+    print(element.text_content())
+
 #
+
+
+
+
+
+
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+#scraperwiki.sqlite.save(unique_keys=['council_reference'], 
+#    data = {
+#    "council_reference": "susan", 
+#    "address": "software developer",
+#    "description": "Description",
+#    "on_notice_to": Date.today,
+#    "date_scraped": Date.today,
+#    "info_url": f"url{id}",
+#    "comment_authority": "COH",
+#    "comment_email": "representation@hobartcity.com.au"
+#    }
+#    )
+
+
+
+
+
+
 #
 # # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+#scraperwiki.sql.select("* from data where 'name'='peter'")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
